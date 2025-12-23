04f6bcac541853ec10c0eae900c05e1e5ebebc51
laraclette <nicosavlar@gmail.com>
2025-12-23 13:57:32 +0100

    scene explorer

--- TestApplication/src/main.cpp
+++ TestApplication/src/main.cpp
@@ -1,23 +1,18 @@
+#include <kotono_common/Path.h>
+#include <kotono_core/Core.h>
 #include <kotono_extension_coretestextension/test.h>
 #include <kotono_extension_editortestextension/test.h>
-#include <kotono_common/Path.h>
 #include <kotono_platform/Window.h>
-#include <kotono_core/Core.h>
 #ifdef EDITOR
     #include <kotono_editor/Editor.h>
 #endif
 
-void init_project_path()
-{
-    Path.projectPath_ = PROJECT_DIRECTORY;
-}
+std::filesystem::path KtPath::projectPath_{ PROJECT_DIRECTORY };
 
 int main()
 {
     say_core();
     say_editor();
-
-    init_project_path();
 
     Core.Init();
 #   ifdef EDITOR
