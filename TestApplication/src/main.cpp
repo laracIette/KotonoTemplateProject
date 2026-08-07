d545229f25b32543e75aeacac061cc0341602f16
laracIette <nicosavlar@gmail.com>
2026-08-07 11:43:23 +0200

    refac: application class
    moved core and editor logic to the application class in the kotono engine

--- TestApplication/src/main.cpp
+++ TestApplication/src/main.cpp
@@ -1,14 +1,7 @@
+#include <kotono_application/Application.h>
 #include <kotono_common/PathManager.h>
-#include <kotono_core/Core.h>
 #include <kotono_extension_testcoreextension/test.h>
 #include <kotono_extension_testeditorextension/test.h>
-#include <kotono_platform/Window.h>
-#ifdef EDITOR
-    #include <kotono_editor/Editor.h>
-#endif
-#ifndef NDEBUG
-    #include <kotono_object/Object.h>
-#endif
 
 std::filesystem::path SPathManager::projectPath_{ PROJECT_DIRECTORY };
 
@@ -17,27 +10,7 @@ int main()
     say_core();
     say_editor();
 
-    Core.Init();
-#   ifdef EDITOR
-        Editor.Init();
-#   endif
-
-    while (!Window.GetShouldClose())
-    {
-        Core.Update();
-#       ifdef EDITOR
-            Editor.Update();
-#       endif
-    }
-
-#   ifdef EDITOR
-        Editor.Cleanup();
-#   endif
-    Core.Cleanup();
-
-#   ifndef NDEBUG
-    KObject::CheckDebugRegistry();
-#   endif
+    UApplication{}.Run();
 
 	return 0;
 }
